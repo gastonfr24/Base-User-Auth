1aae0aac6466ce8c4f0f4e458955a2a0c69c9541
gastonfr24 <gastonfr24@gmail.com>
2023-10-04 17:19:14 -0300

    update: user model | superuser creation

--- apps/user/models.py
+++ apps/user/models.py
@@ -4,42 +4,50 @@ from django.utils import timezone
 import uuid
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        
+    def create_user(self, email, password=None, **kwargs):
         if not email:
-            raise ValueError('El campo Email es obligatorio.')
-        
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        email = email.lower()
+
+        user = self.model(
+            email=email,
+            **kwargs
+        )
+
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, email, password=None, **kwargs):
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('El superusuario debe tener is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('El superusuario debe tener is_superuser=True.')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-        return self.create_user(email, password, **extra_fields)
+        return user
 
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True)
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, max_length=255)
+
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(default=timezone.now)
 
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email

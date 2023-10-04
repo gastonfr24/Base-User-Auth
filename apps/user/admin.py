fae39bebbd0c33b41ac55d57046b0720a5b7498e
gastonfr24 <gastonfr24@gmail.com>
2023-10-04 17:20:04 -0300

    new-feature: admin panel

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserAccount, UserProfile
 
-# Register your models here.
+# Define el admin para UserAccount
+class UserAccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información personal', {'fields': ('first_name', 'last_name')}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+# Define el admin para UserProfile
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name')
+
+# Registra los modelos en el panel de administración
+admin.site.register(UserAccount, UserAccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
